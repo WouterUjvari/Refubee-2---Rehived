1abb8a4492da413a0bd3765243e8861fa4aae67a
DwellerDev <wouter.ujvari@gmail.com>
2024-08-15 22:46:01 +0200

    Revert "trying chatgpt code"
    This reverts commit 87b1642fb18454a449f2e7547448c5d146dda2d4.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,171 +1,258 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+
+namespace TarodevController
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 10f;
-    public Transform groundCheck;
-    public LayerMask groundLayer;
-    public float slopeCheckDistance = 0.5f;
-    public float maxSlopeAngle = 45f;
-    public PhysicsMaterial2D noFrictionMaterial;
-    public PhysicsMaterial2D fullFrictionMaterial;
-    public Animator animator;
-
-    private Rigidbody2D rb;
-    private bool isGrounded;
-    private bool isOnSlope;
-    private float horizontalInput;
-    private Vector2 slopeNormalPerp;
-    private float slopeDownAngle;
-    private float slopeDownAngleOld;
-    private bool canWalkOnSlope;
-
-    void Start()
+    [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+    public class PlayerController : MonoBehaviour, IPlayerController
     {
-        rb = GetComponent<Rigidbody2D>();
-    }
+        [SerializeField] private ScriptableStats _stats;
+        [SerializeField] private Animator _animator;
+        private Rigidbody2D _rb;
+        private CapsuleCollider2D _col;
+        private FrameInput _frameInput;
+        private Vector2 _frameVelocity;
+        private bool _cachedQueryStartInColliders;
 
-    void Update()
-    {
-        // Get horizontal input
-        horizontalInput = Input.GetAxis("Horizontal");
+        #region Interface
 
-        // Check if the player is on the ground
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+        public Vector2 FrameInput => _frameInput.Move;
+        public event Action<bool, float> GroundedChanged;
+        public event Action Jumped;
 
-        // Jump
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        #endregion
+
+        private float _time;
+
+        private void Awake()
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            _rb = GetComponent<Rigidbody2D>();
+            _col = GetComponent<CapsuleCollider2D>();
+
+            _cachedQueryStartInColliders = Physics2D.queriesStartInColliders;
         }
-    }
 
-    void FixedUpdate()
-    {
-        // Slope handling
-        SlopeCheck();
-        HandleAnimation();
-        FaceInput();
-
-        if (isOnSlope && canWalkOnSlope)
+        private void Update()
         {
-            rb.velocity = new Vector2(horizontalInput * moveSpeed * slopeNormalPerp.x, horizontalInput * moveSpeed * slopeNormalPerp.y);
+            _time += Time.deltaTime;
+            GatherInput();
+            FaceInput();
+            HandleAnimation();
         }
-        else if (isGrounded)
+
+        private void GatherInput()
         {
-            rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
-        }
-        else
-        {
-            rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
-        }
-    }
-
-    void SlopeCheck()
-    {
-        Vector2 checkPos = transform.position + new Vector3(0.0f, 0.5f);
-        SlopeCheckHorizontal(checkPos);
-        SlopeCheckVertical(checkPos);
-    }
-
-    void SlopeCheckHorizontal(Vector2 checkPos)
-    {
-        RaycastHit2D slopeHitFront = Physics2D.Raycast(checkPos, transform.right, slopeCheckDistance, groundLayer);
-        RaycastHit2D slopeHitBack = Physics2D.Raycast(checkPos, -transform.right, slopeCheckDistance, groundLayer);
-
-        if (slopeHitFront || slopeHitBack)
-        {
-            isOnSlope = true;
-            slopeNormalPerp = Vector2.Perpendicular(slopeHitFront ? slopeHitFront.normal : slopeHitBack.normal).normalized;
-
-            slopeDownAngle = Vector2.Angle(slopeHitFront ? slopeHitFront.normal : slopeHitBack.normal, Vector2.up);
-
-            if (slopeDownAngle <= maxSlopeAngle)
+            _frameInput = new FrameInput
             {
-                canWalkOnSlope = true;
-                rb.sharedMaterial = fullFrictionMaterial;
+                JumpDown = Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.C),
+                JumpHeld = Input.GetButton("Jump") || Input.GetKey(KeyCode.C),
+                Move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"))
+            };
+
+            if (_stats.SnapInput)
+            {
+                _frameInput.Move.x = Mathf.Abs(_frameInput.Move.x) < _stats.HorizontalDeadZoneThreshold ? 0 : Mathf.Sign(_frameInput.Move.x);
+                _frameInput.Move.y = Mathf.Abs(_frameInput.Move.y) < _stats.VerticalDeadZoneThreshold ? 0 : Mathf.Sign(_frameInput.Move.y);
+            }
+
+            if (_frameInput.JumpDown)
+            {
+                _jumpToConsume = true;
+                _timeJumpWasPressed = _time;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            CheckCollisions();
+
+            HandleJump();
+            HandleDirection();
+            HandleGravity();
+            
+            ApplyMovement();
+        }
+
+        #region Collisions
+        
+        private float _frameLeftGrounded = float.MinValue;
+        private bool _grounded;
+
+        private void CheckCollisions()
+        {
+            Physics2D.queriesStartInColliders = false;
+
+            // Ground and Ceiling
+            bool groundHit = Physics2D.CapsuleCast(_col.bounds.center, _col.size, _col.direction, 0, Vector2.down, _stats.GrounderDistance, ~_stats.PlayerLayer);
+            bool ceilingHit = Physics2D.CapsuleCast(_col.bounds.center, _col.size, _col.direction, 0, Vector2.up, _stats.GrounderDistance, ~_stats.PlayerLayer);
+
+            // Hit a Ceiling
+            if (ceilingHit) _frameVelocity.y = Mathf.Min(0, _frameVelocity.y);
+
+            // Landed on the Ground
+            if (!_grounded && groundHit)
+            {
+                _grounded = true;
+                _coyoteUsable = true;
+                _bufferedJumpUsable = true;
+                _endedJumpEarly = false;
+                GroundedChanged?.Invoke(true, Mathf.Abs(_frameVelocity.y));
+            }
+            // Left the Ground
+            else if (_grounded && !groundHit)
+            {
+                _grounded = false;
+                _frameLeftGrounded = _time;
+                GroundedChanged?.Invoke(false, 0);
+            }
+
+            Physics2D.queriesStartInColliders = _cachedQueryStartInColliders;
+        }
+
+        #endregion
+
+
+        #region Jumping
+
+        private bool _jumpToConsume;
+        private bool _bufferedJumpUsable;
+        private bool _endedJumpEarly;
+        private bool _coyoteUsable;
+        private float _timeJumpWasPressed;
+
+        private bool HasBufferedJump => _bufferedJumpUsable && _time < _timeJumpWasPressed + _stats.JumpBuffer;
+        private bool CanUseCoyote => _coyoteUsable && !_grounded && _time < _frameLeftGrounded + _stats.CoyoteTime;
+
+        private void HandleJump()
+        {
+            if (!_endedJumpEarly && !_grounded && !_frameInput.JumpHeld && _rb.velocity.y > 0) _endedJumpEarly = true;
+
+            if (!_jumpToConsume && !HasBufferedJump) return;
+
+            if (_grounded || CanUseCoyote) ExecuteJump();
+
+            _jumpToConsume = false;
+        }
+
+        private void ExecuteJump()
+        {
+            _endedJumpEarly = false;
+            _timeJumpWasPressed = 0;
+            _bufferedJumpUsable = false;
+            _coyoteUsable = false;
+            _frameVelocity.y = _stats.JumpPower;
+            Jumped?.Invoke();
+        }
+
+        #endregion
+
+        #region Horizontal
+
+        private void HandleDirection()
+        {
+            if (_frameInput.Move.x == 0)
+            {
+                var deceleration = _grounded ? _stats.GroundDeceleration : _stats.AirDeceleration;
+                _frameVelocity.x = Mathf.MoveTowards(_frameVelocity.x, 0, deceleration * Time.fixedDeltaTime);
             }
             else
             {
-                canWalkOnSlope = false;
-                rb.sharedMaterial = noFrictionMaterial;
-            }
-        }
-        else
-        {
-            isOnSlope = false;
-            canWalkOnSlope = false;
-            rb.sharedMaterial = noFrictionMaterial;
-        }
-    }
-
-    void SlopeCheckVertical(Vector2 checkPos)
-    {
-        RaycastHit2D hit = Physics2D.Raycast(checkPos, Vector2.down, slopeCheckDistance, groundLayer);
-
-        if (hit)
-        {
-            slopeDownAngleOld = slopeDownAngle;
-            slopeNormalPerp = Vector2.Perpendicular(hit.normal).normalized;
-            slopeDownAngle = Vector2.Angle(hit.normal, Vector2.up);
-
-            if (slopeDownAngle != slopeDownAngleOld)
-            {
-                isOnSlope = true;
+                _frameVelocity.x = Mathf.MoveTowards(_frameVelocity.x, _frameInput.Move.x * _stats.MaxSpeed, _stats.Acceleration * Time.fixedDeltaTime);
             }
         }
 
-        if (slopeDownAngle == 0.0f)
-        {
-            isOnSlope = false;
-        }
-    }
+        #endregion
 
-    void FaceInput()
-    {
-        if (horizontalInput > 0.1f)
-        {
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-        if (horizontalInput < -0.1f)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
-    }
+        #region Gravity
 
-    void HandleAnimation()
-    {
-        if (isGrounded)
+        private void HandleGravity()
         {
-            if (horizontalInput != 0)
+            if (_grounded && _frameVelocity.y <= 0f)
             {
-                ChangeAnimation("WALK");
+                _frameVelocity.y = _stats.GroundingForce;
             }
             else
             {
-                ChangeAnimation("IDLE");
+                var inAirGravity = _stats.FallAcceleration;
+                if (_endedJumpEarly && _frameVelocity.y > 0) inAirGravity *= _stats.JumpEndEarlyGravityModifier;
+                _frameVelocity.y = Mathf.MoveTowards(_frameVelocity.y, -_stats.MaxFallSpeed, inAirGravity * Time.fixedDeltaTime);
             }
         }
-        else
+
+        #endregion
+
+        void FaceInput()
         {
-            if (rb.velocity.y > 0.1f)
+            if (FrameInput.x > 0.1f)
             {
-                ChangeAnimation("JUMP");
+                transform.localScale = new Vector3(1, 1, 1);
             }
-            else if (rb.velocity.y < -0.1f)
+            if (FrameInput.x < -0.1f)
             {
-                ChangeAnimation("FALL");
+                transform.localScale = new Vector3(-1, 1, 1);
             }
         }
+
+        void HandleAnimation()
+        {
+            if (_grounded)
+            {
+                if (FrameInput.x != 0)
+                {
+                    ChangeAnimation("WALK");
+                }
+                else
+                {
+                    ChangeAnimation("IDLE");
+                }
+            }
+            else
+            {
+                if (_rb.velocity.y > 0.1f)
+                {
+                    ChangeAnimation("JUMP");
+                }
+                else if (_rb.velocity.y < -0.1f)
+                {
+                    ChangeAnimation("FALL");
+                }
+            }
+        }
+        public void ChangeAnimation(string animName)
+        {
+            if (_animator.GetCurrentAnimatorClipInfo(0)[0].clip.name != animName)
+            {
+                _animator.Play(animName);
+            }
+        }
+        #region Animations
+
+
+
+        #endregion
+        private void ApplyMovement() => _rb.velocity = _frameVelocity;
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (_stats == null) Debug.LogWarning("Please assign a ScriptableStats asset to the Player Controller's Stats slot", this);
+        }
+#endif
     }
-    public void ChangeAnimation(string animName)
+
+    public struct FrameInput
     {
-        if (animator.GetCurrentAnimatorClipInfo(0)[0].clip.name != animName)
-        {
-            animator.Play(animName);
-        }
+        public bool JumpDown;
+        public bool JumpHeld;
+        public Vector2 Move;
+    }
+
+    public interface IPlayerController
+    {
+        public event Action<bool, float> GroundedChanged;
+
+        public event Action Jumped;
+        public Vector2 FrameInput { get; }
     }
 }
